9768152e7ff4c9f0347d3930fabfaac5c99a7c26
gubinap <gubinap@mos-team.ru>
2023-12-15 13:44:08 +0300

    TEST-TASK fixed Airplane model

--- airlines/models.py
+++ airlines/models.py
@@ -3,7 +3,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Airplane(models.Model):
-    plain_identifier = models.IntegerField(
+    airplane_identifier = models.IntegerField(
         validators=[
             MaxValueValidator(10),
             MinValueValidator(1)
@@ -13,6 +13,6 @@ class Airplane(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.plain_identifier} | ' \
+        return f'{self.airplane_identifier} | ' \
                f'{self.passenger_capacity} | ' \
                f'{self.created}'
