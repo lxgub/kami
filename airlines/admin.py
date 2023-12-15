9cec7dbf187e61cb2d37fde32fdc3482bcebff1d
gubinap <gubinap@mos-team.ru>
2023-12-15 11:36:34 +0300

    TEST-TASK migration added (creation model Airplane)

--- airlines/admin.py
+++ airlines/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from airlines.models import Airplane
+
+admin.site.register(Airplane)
--- airlines/models.py
+++ airlines/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Airplane(models.Model):
+    plain_identifier = models.IntegerField(
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ]
+    )
+    passenger_capacity = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.plain_identifier} | ' \
+               f'{self.passenger_capacity} | ' \
+               f'{self.created}'
